4c6658073e8b3e1d3142599e70f4385cd8edfee3
Jon Lee <metrenome@gmail.com>
2020-03-06 18:29:10 -0500

    Add Swiper to ChampionBase
    Added Swiper to ChampionBase and made it work. Next step is to add some actions to get the champion data and create a database for the champion item sets and rune pages.

--- src/components/champions/ChampionBase.js
+++ src/components/champions/ChampionBase.js
@@ -1,12 +1,87 @@
 import React from 'react';
-import {
-    useParams
-  } from "react-router-dom";
+import Swiper from 'swiper';
+import 'swiper/css/swiper.min.css';
+import { useParams } from 'react-router-dom';
 
-const ChampionBase = () => {
-    let { id } = useParams();
-    console.log(`ChampionBase sees ${id}`);
-    return <div>ChampionBase</div>;
-};
+import ChampionCard from '../ChampionCard';
+
+class ChampionBase extends React.Component {
+  componentDidMount() {
+    var swiper = new Swiper('.swiper-container', {
+      navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+      },
+      pagination: {
+        el: '.swiper-pagination',
+      },
+      freeMode: {
+        freemode: 'true',
+      }
+    });
+  }
+  
+  render() {
+    return (
+      <div className="swiper-container">
+         <div className="swiper-wrapper">
+           <div className="swiper-slide">
+             Slide 1
+             {/* <ChampionCard key={champion.key}
+                name={champion.name} 
+                id={champion.key}
+                image={champion.image}
+              /> */}
+            </div>
+           <div className="swiper-slide">Slide 2</div>
+           <div className="swiper-slide">Slide 3</div>
+           <div className="swiper-slide">Slide 4</div>
+           <div className="swiper-slide">Slide 5</div>
+           <div className="swiper-slide">Slide 6</div>
+           <div className="swiper-slide">Slide 7</div>
+           <div className="swiper-slide">Slide 8</div>
+           <div className="swiper-slide">Slide 9</div>
+           <div className="swiper-slide">Slide 10</div>
+         </div>
+         <div className="swiper-pagination"></div>
+         <div className="swiper-button-prev"></div>
+         <div className="swiper-button-next"></div>
+       </div>
+    );
+  }
+}
+// const ChampionBase = () => {
+//     let { id } = useParams();
+//     var SwiperContainer = new Swiper('.swiper-container', {
+//       navigation: {
+//         nextEl: '.swiper-button-next',
+//         prevEl: '.swiper-button-prev',
+//       },
+//       pagination: {
+//         el: '.swiper-pagination',
+//       }
+//     });
+
+//     console.log(`ChampionBase sees ${id}`);
+//     return (
+//       <div class="swiper-container">
+//         <div class="swiper-wrapper">
+//           <div class="swiper-slide">Slide 1</div>
+//           <div class="swiper-slide">Slide 2</div>
+//           <div class="swiper-slide">Slide 3</div>
+//           <div class="swiper-slide">Slide 4</div>
+//           <div class="swiper-slide">Slide 5</div>
+//           <div class="swiper-slide">Slide 6</div>
+//           <div class="swiper-slide">Slide 7</div>
+//           <div class="swiper-slide">Slide 8</div>
+//           <div class="swiper-slide">Slide 9</div>
+//           <div class="swiper-slide">Slide 10</div>
+//         </div>
+//         <div class="swiper-pagination"></div>
+//         <div class="swiper-button-prev"></div>
+//         <div class="swiper-button-next"></div>
+//       </div>
+//     );
+// };
 
 export default ChampionBase;
